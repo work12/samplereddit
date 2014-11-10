e3fcaca6b376c618037a712088db70becf860b76
Eesha K Reddy <eeshakreddy09@gmail.com>
2014-11-10 19:30:23 +0530

    Finish Sign Up

---
+++ samplereddit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samplereddit::Application.config.secret_token = '6c128e7bee829e5840278f0497751718c6eadd2e0d337cdc64d38ec3ecccf8f2bfb89da700ced5758ef441a1bae60ab9eae8901aa35069a82caf4822b7c5fb66'
